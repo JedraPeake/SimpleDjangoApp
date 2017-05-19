a1e483858e901188fc782f4ab7abe195cbada1ef
JedraPeake <jpeake2@uwo.ca>
2017-05-18 22:53:36 -0400

    edited views and urls

--- web/landingPage/views.py
+++ web/landingPage/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 # Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
